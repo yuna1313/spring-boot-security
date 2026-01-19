813bf39c8e0e5d73ef88d37bb6726a82e7398c9d
yuna1313 <93983333+yuna1313@users.noreply.github.com>
2026-01-19 20:56:02 +0900

    메서드 보안 활성화를 위해 EnableMethodSecurity 추가

--- src/main/java/com/cos/security/config/SecurityConfig.java
+++ src/main/java/com/cos/security/config/SecurityConfig.java
@@ -2,13 +2,19 @@ package com.cos.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+// Spring Boot + Spring Security 6 이후부터는 기본 SecurityFilterChain이 자동 구성되기 때문에 @EnableWebSecurity를 직접 선언하지 않아도 보안 필터 체인이 활성화
 @EnableWebSecurity
+// EnableGlobalMethodSecurity 는 deprecated 되어서 EnableMethodSecurity 사용
+// Secured 어노테이션 활성화, preAuthorize 어노테이션 활성화 (기본이 true임), prePostEnabled는 deprecated 되어 기본으로 true임
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
--- src/main/java/com/cos/security/controller/IndexController.java
+++ src/main/java/com/cos/security/controller/IndexController.java
@@ -3,6 +3,9 @@ package com.cos.security.controller;
 import com.cos.security.model.User;
 import com.cos.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -59,5 +62,17 @@ public class IndexController {
         userRepository.save(user);
 
         return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/data")
+    public @ResponseBody String data() {
+        return "데이터정보";
     }
 }
